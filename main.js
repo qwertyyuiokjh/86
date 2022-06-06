4a7c704f1997516b44c57bb8c332b9b4b1791233
qwertyyuiokjh <105309213+qwertyyuiokjh@users.noreply.github.com>
2022-06-06 16:51:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.image.fromURL("Birthday_Image.jpg", function(img){
+	block_image_object.img;})
+    {
+        player_object.scaleToWidth(700);
+        player_object.scaleToWidth(510);
+        player_object.set({
+            top:0,
+            top:1
+        });
+        canvas.add(block_image_object);
+    };
+	
+}
+
+function playSound(){
+	x.play();
+}
